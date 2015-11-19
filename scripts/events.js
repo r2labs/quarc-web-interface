e3e17c594a2bfe1a3869bb06cb4c584bae4794a4
Hershal Bhave <hershal.bhave@gmail.com>
2015-11-19 14:46:18 -0600

    default argument fixup

--- scripts/events.js
+++ scripts/events.js
@@ -1,10 +1,9 @@
 var event;
 
 function goto(x, y, z, ga) {
-    ga = ga || -90;
+    if (ga == undefined) { ga = -90; }
     jQuery.ajax({
         url: 'http://localhost:8080/goto' + '?x=' + x + "&y=" + y + "&z=" + z + "&ga_d=" + ga,
-        // async: true
     });
 }
 
