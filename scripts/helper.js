9a0d1bb0b55f9fc4ac1440c62f7c35ebe03fa0b3
Hershal Bhave <hershal.bhave@gmail.com>
2015-11-19 04:49:20 -0600

    use jquery to throttle mouse events to 30fps

--- scripts/helper.js
+++ scripts/helper.js
@@ -11,3 +11,26 @@ function lerp(x, x_min, x_max, y_min, y_max) {
     else if (x < x_min) { return y_min; }
     return y_min + ((y_max - y_min)*(x- x_min))/(x_max - x_min);
 }
+
+function throttle(fn, threshhold, scope) {
+    threshhold || (threshhold = 250);
+    var last,
+        deferTimer;
+    return function () {
+        var context = scope || this;
+
+        var now = +new Date,
+            args = arguments;
+        if (last && now < last + threshhold) {
+            // hold on to it
+            clearTimeout(deferTimer);
+            deferTimer = setTimeout(function () {
+                last = now;
+                fn.apply(context, args);
+            }, threshhold);
+        } else {
+            last = now;
+            fn.apply(context, args);
+        }
+    };
+}
