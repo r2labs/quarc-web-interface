9ed470e29cb07738b6182b1cdb8d6da73413cef0
Hershal Bhave <hershal.bhave@gmail.com>
2015-11-19 19:49:27 -0600

    added robot control panel

---
+++ scripts/robocontrol.js
@@ -0,0 +1,9 @@
+function robocontrol_pick() {
+    console.log("picking");
+    pick(grid_obj.flairdown.x, grid_obj.flairdown.y, grid_obj.flairdown.z);
+}
+
+function robocontrol_place() {
+    console.log("placing");
+    place(grid_obj.flairdown.x, grid_obj.flairdown.y, grid_obj.flairdown.z);
+}
