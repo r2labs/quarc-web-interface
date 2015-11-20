31607400184b045be39b24e9626fd45ed09890bf
Hershal Bhave <hershal.bhave@gmail.com>
2015-11-19 19:47:42 -0600

    added half-baked camera controls

---
+++ scripts/camera.js
@@ -0,0 +1,23 @@
+function camera_btn_up() {
+    $("#monitor").css("width", parseInt($("#monitor").css("width"))+50 + "px");
+}
+
+function camera_btn_down() {
+    $("#monitor").css("width", parseInt($("#monitor").css("width"))-50 + "px");
+}
+
+function camera_btn_left() {
+
+}
+
+function camera_btn_right() {
+
+}
+
+function camera_zoomin_btn() {
+
+}
+
+function camera_zoomout_btn() {
+
+}
