e32a6a758ecfee4907081201cb62a8d1c11f2ce0
Hershal Bhave <hershal.bhave@gmail.com>
2015-11-22 18:57:48 -0600

    robocontrol bugfix

--- scripts/robocontrol.js
+++ scripts/robocontrol.js
@@ -1,9 +1,13 @@
 function robocontrol_pick() {
+    x = grid_obj.flairmove.x;
+    y = grid_obj.flairmove.y;
     console.log("picking at x: " + x + " y: " + y);
     pick(x, y, 5, -90);
 }
 
 function robocontrol_place() {
+    x = grid_obj.flairmove.x;
+    y = grid_obj.flairmove.y;
     console.log("placing at x: " + x + " y: " + y);
     place(x, y, 5, -90);
 }
